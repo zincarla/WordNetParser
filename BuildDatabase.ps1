157df560878a2c5d2487e571293ff2ea83277cd8
ZiviZ <tal-skala@ziviz.us>
2018-04-26 10:27:15 -0400

    Initial

---
+++ BuildDatabase.ps1
@@ -0,0 +1,146 @@
+﻿#Create a new database, and create a user for it
+#Then run the following
+Param($Data, $SQLIP="localhost", $UserID="root", $UserPWD, $Database="WordNetDictionary", $SSL=$true)
+
+#From Thomas Maurer
+#https://www.thomasmaurer.ch/2011/04/powershell-run-mysql-querys-with-powershell/
+Function Run-MySQLQuery {
+<#
+.SYNOPSIS
+   run-MySQLQuery
+    
+.DESCRIPTION
+   By default, this script will:
+    - Will open a MySQL Connection
+	- Will Send a Command to a MySQL Server
+	- Will close the MySQL Connection
+	This function uses the MySQL .NET Connector or MySQL.Data.dll file
+     
+.PARAMETER ConnectionString
+    Adds the MySQL Connection String for the specific MySQL Server
+     
+.PARAMETER Query
+ 
+    The MySQL Query which should be send to the MySQL Server
+	
+.EXAMPLE
+    C:\PS> run-MySQLQuery -ConnectionString "Server=localhost;Uid=root;Pwd=p@ssword;database=project;" -Query "SELECT * FROM firsttest" 
+    
+    Description
+    -----------
+    This command run the MySQL Query "SELECT * FROM firsttest" 
+	to the MySQL Server "localhost" with the Credentials User: Root and password: p@ssword and selects the database project
+         
+.EXAMPLE
+    C:\PS> run-MySQLQuery -ConnectionString "Server=localhost;Uid=root;Pwd=p@ssword;database=project;" -Query "UPDATE firsttest SET firstname='Thomas' WHERE Firstname like 'PAUL'" 
+    
+    Description
+    -----------
+    This command run the MySQL Query "UPDATE project.firsttest SET firstname='Thomas' WHERE Firstname like 'PAUL'" 
+	to the MySQL Server "localhost" with the Credentials User: Root and password: p@ssword
+	
+.EXAMPLE
+    C:\PS> run-MySQLQuery -ConnectionString "Server=localhost;Uid=root;Pwd=p@ssword;" -Query "UPDATE project.firsttest SET firstname='Thomas' WHERE Firstname like 'PAUL'" 
+    
+    Description
+    -----------
+    This command run the MySQL Query "UPDATE project.firsttest SET firstname='Thomas' WHERE Firstname like 'PAUL'" 
+	to the MySQL Server "localhost" with the Credentials User: Root and password: p@ssword and selects the database project
+    
+#>
+	Param(
+        [Parameter(
+            Mandatory = $true,
+            ParameterSetName = '',
+            ValueFromPipeline = $true)]
+            [string]$query,   
+		[Parameter(
+            Mandatory = $true,
+            ParameterSetName = '',
+            ValueFromPipeline = $true)]
+            [string]$connectionString
+        )
+	Begin {
+		Write-Verbose "Starting Begin Section"		
+    }
+	Process {
+		Write-Verbose "Starting Process Section"
+		try {
+			# load MySQL driver and create connection
+			Write-Verbose "Create Database Connection"
+			# You could also could use a direct Link to the DLL File
+			# $mySQLDataDLL = "C:\scripts\mysql\MySQL.Data.dll"
+			# [void][system.reflection.Assembly]::LoadFrom($mySQLDataDLL)
+			[void][System.Reflection.Assembly]::LoadWithPartialName("MySql.Data")
+			$connection = New-Object MySql.Data.MySqlClient.MySqlConnection
+			$connection.ConnectionString = $ConnectionString
+			Write-Verbose "Open Database Connection"
+			$connection.Open()
+			
+			# Run MySQL Querys
+			Write-Verbose "Run MySQL Querys"
+			$command = New-Object MySql.Data.MySqlClient.MySqlCommand($query, $connection)
+			$dataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($command)
+			$dataSet = New-Object System.Data.DataSet
+			$recordCount = $dataAdapter.Fill($dataSet, "data")
+			$dataSet.Tables["data"] | Format-Table
+		}		
+		catch {
+			Write-Host "Could not run MySQL Query" $Error[0]
+            Write-Host $query
+		}	
+		Finally {
+			Write-Verbose "Close Connection"
+			$connection.Close()
+		}
+    }
+	End {
+		Write-Verbose "Starting End Section"
+	}
+}
+
+$ConnectionString = "Server=$SQLIP;Uid=$UserID;Pwd=$UserPWD;database=$Database;Encrypt=$SSL"
+
+$TableCreateQueries = @(
+    "CREATE TABLE Words (ID BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE, Word VARCHAR(255) NOT NULL, Glossary VARCHAR(2048), SynSetID BIGINT, Type VARCHAR(1) NOT NULL);",
+    "CREATE TABLE Relations (ID BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE, SourceID BIGINT NOT NULL, PairedID BIGINT NOT NULL, Relationship VARCHAR(2) NOT NULL, PairedType VARCHAR(1) NOT NULL);",
+    "CREATE INDEX Word ON Words (Word);",
+    "CREATE INDEX SynSetID ON Words (SynSetID);",
+    "CREATE INDEX SourceID ON Relations (SourceID);",
+    "CREATE INDEX PairedID ON Relations (PairedID);",
+    "CREATE INDEX Type ON Words (Type);",
+    "CREATE INDEX Type ON Relations (PairedType);"
+)
+
+foreach ($Query in $TableCreateQueries) {
+    Run-MySQLQuery -ConnectionString $ConnectionString -query $Query
+}
+
+
+$LastUpdate = [DateTime]::Now
+$UpdateSchedule = [TimeSpan]::FromSeconds(5)
+$I=0;
+foreach ($Word in $Data["Words"]) {
+    $Name = $Word.Name.Replace("'", "\'")
+    $Description = $Word.Glossary.Replace("'", "\'").Trim()
+    $Query = "INSERT INTO Words (Word, Glossary, SynSetID, Type) VALUES ('$Name', '$Description', $($Word.ID), '$($Word.Type)');"
+    Run-MySQLQuery -ConnectionString $ConnectionString -query $Query
+    $I++;
+    if ([DateTime]::Now - $LastUpdate -gt $UpdateSchedule) {
+        Write-Host "$($I)/$($Data["Words"].Count)"
+        $LastUpdate = [DateTime]::Now
+    }
+}
+Write-Host "On to relations"
+$I=0;
+foreach ($Relation in $Data["Relations"]) {
+    $Relationship = $Relation.Relationship.Replace("\", "\\")
+    $Query = "INSERT INTO Relations (SourceID, PairedID, Relationship, PairedType) VALUES ($($Relation.SourceID), $($Relation.PairedID), '$($Relationship)', '$($Relation.PairedType)');"
+    Run-MySQLQuery -ConnectionString $ConnectionString -query $Query
+    $I++;
+    if ([DateTime]::Now - $LastUpdate -gt $UpdateSchedule) {
+        Write-Host "$($I)/$($Data["Relations"].Count)"
+        $LastUpdate = [DateTime]::Now
+    }
+}
+Write-Host "Done"
---
+++ WordNetParser.ps1
@@ -0,0 +1,104 @@
+﻿Param($SkipLoad = 29, $Paths=(".\data.noun", ".\data.verb",".\data.adv",".\data.adj",".\data.adv"))
+                # ID    lexnum type  count          Word        lexid  pntrcnt    pntrsmbl id typ src/trgt  frame:cnt   fnum  wnum           glossary
+$RegexPattern = "^(\d+) (\d\d) (\w) (\S{2})(?: (\S+ \S))+ (\d\d\d)(?: (.{1,2} \d+ \w \S+))* ?(\d\d( \+ \d\d \S{2})*)? \| (.*)$"
+$Matcher = [regex]::new($RegexPattern)
+#https://wordnet.princeton.edu/documentation/wndb5wn
+#https://wordnet.princeton.edu/documentation/wninput5wn
+<#
+The pointer_symbol s for nouns are:
+
+!    Antonym 
+@    Hypernym 
+@i    Instance Hypernym 
+ ~    Hyponym 
+ ~i    Instance Hyponym 
+#m    Member holonym 
+#s    Substance holonym 
+#p    Part holonym 
+%m    Member meronym 
+%s    Substance meronym 
+%p    Part meronym 
+=    Attribute 
++    Derivationally related form         
+;c    Domain of synset - TOPIC 
+-c    Member of this domain - TOPIC 
+;r    Domain of synset - REGION 
+-r    Member of this domain - REGION 
+;u    Domain of synset - USAGE 
+-u    Member of this domain - USAGE 
+The pointer_symbol s for verbs are:
+
+!    Antonym 
+@    Hypernym 
+ ~    Hyponym 
+*    Entailment 
+>    Cause 
+^    Also see 
+$    Verb Group 
++    Derivationally related form         
+;c    Domain of synset - TOPIC 
+;r    Domain of synset - REGION 
+;u    Domain of synset - USAGE 
+The pointer_symbol s for adjectives are:
+
+!    Antonym 
+&    Similar to 
+<    Participle of verb 
+\    Pertainym (pertains to noun) 
+=    Attribute 
+^    Also see 
+;c    Domain of synset - TOPIC 
+;r    Domain of synset - REGION 
+;u    Domain of synset - USAGE 
+The pointer_symbol s for adverbs are:
+
+!    Antonym 
+\    Derived from adjective 
+;c    Domain of synset - TOPIC 
+;r    Domain of synset - REGION 
+;u    Domain of synset - USAGE 
+#>
+
+$WordResults = New-Object System.Collections.ArrayList
+$RelationResults = New-Object System.Collections.ArrayList
+
+$LastUpdate = [DateTime]::Now
+$UpdateSchedule = [TimeSpan]::FromSeconds(5)
+$Trash = $null
+
+foreach ($Path in $Paths) {
+    Write-Host $Path
+    $Content = Get-Content -Path $Path
+
+    for ($I = $SkipLoad; $I-lt $Content.Length; $I++) {
+        $Line = $Content[$I]
+
+        $RMatches = $Matcher.Matches($Line)[0]
+        if ($RMatches -eq $null) {
+            Write-Host "ERR: $Line"
+        }
+        $ID = $RMatches.Groups[1].Captures.Value
+        $Type = $RMatches.Groups[3].Captures.Value
+        $Names = $RMatches.Groups[5].Captures.Value
+        $Relations = $RMatches.Groups[7].Captures.Value
+        $Glossary = $RMatches.Groups[10].Captures.Value
+
+        foreach ($Name in $Names) {
+            $Trash=$WordResults.Add((New-Object -TypeName PSObject -Property @{Name=$Name.Substring(0, $Name.Length-2); ID=$ID; Glossary=$Glossary; Type=$Type })) # Piping to out-null is actually really slow. idk why. Setting a trash variable is waaaay faster, need to do this or results are f*cked
+        }
+
+        foreach ($Relation in $Relations) {
+            $RelationParts = $Relation.Split(" ")
+            $PairType = $RelationParts[0]
+            $PairedID = $RelationParts[1]
+            $PairedType = $RelationParts[2]
+            $SourceTarget = $RelationParts[3]
+            $Trash=$RelationResults.Add((New-Object -TypeName PSObject -Property @{Relationship=$PairType;SourceID=$ID;PairedID=$PairedID;PairedType=$PairedType; SourceTarget=$SourceTarget })) # Piping to out-null is actually really slow. idk why. Setting a trash variable is waaaay faster, need to do this or results are f*cked
+        }
+        if ([DateTime]::Now - $LastUpdate -gt $UpdateSchedule) {
+            Write-Host "$I/$($Content.Length)"
+            $LastUpdate = [DateTime]::Now
+        }
+    }
+}
+return @{Words=$WordResults;Relations=$RelationResults}
